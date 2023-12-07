96a7037de4e202b92218a2da995db417bd001312
Shrey Sharma <shreysharmar619@gmail.com>
2023-12-07 23:14:58 +0530

    modified files

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
--- src/Components/items/Item.jsx
+++ src/Components/items/Item.jsx
@@ -8,10 +8,10 @@ const Item = (props) => {
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
-        {props.new_price}
+        Rs.{props.new_price}
         </div>
         <div className="item-price-old">
-        {props.old_price}
+        Rs.{props.old_price}
         </div>
       </div>
     </div>
